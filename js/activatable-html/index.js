d63a62c1ef2e5e40eece2014256398168e4bb17b
Anthony Toniolli <anthony.toniolli@totvs.com.br>
2021-07-07 17:29:15 -0300

    feat: Added auxialiry classes

--- js/activatable-html/index.js
+++ js/activatable-html/index.js
@@ -1,3 +1,4 @@
+// On Document ready
 document.addEventListener(
   "DOMContentLoaded",
   function () {
@@ -7,20 +8,31 @@ document.addEventListener(
 );
 
 function loadActivators() {
+  // Get all activators elements
   var activators = document.querySelectorAll("[activates]");
+
+  // For each element
   activators.forEach((el) => {
+
+    // On click
     el.addEventListener("click", () => {
+
+      // Get element which will be activated
       const targetElement = document.getElementById(
         el.getAttribute("activates")
       );
+
+      // Get callback function 
       const callback = el.getAttribute("callback");
 
+      // Toggle activated class
       if (targetElement.classList.contains("activated")) {
         targetElement.classList.remove("activated");
       } else {
         targetElement.classList.add("activated");
       }
 
+      // Call cllback function if any
       if (callback) {
         window[callback]();
       }
@@ -28,10 +40,7 @@ function loadActivators() {
   });
 }
 
-function activatableFuntions() {
-  toggleSideMenu();
-}
-
+// Side menu activator callback function
 function toggleSideMenu() {
   const root = document.querySelector(":root");
   if (document.querySelectorAll(".side-menu.activated").length > 0) {
